36549e16a179cca91ddd7bc5d8c13ff616a6cf29
austinabell <austinabell8@gmail.com>
2021-03-25 14:07:56 -0400

    fix reference

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import "regenerator-runtime/runtime";
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
-import { Chess } from "chess.js";
+import Chess from "chess.js";
 import { Button, Typography, Modal } from "@material-ui/core";
 import SignIn from "./components/SignIn";
 import { makeStyles } from "@material-ui/core/styles";
